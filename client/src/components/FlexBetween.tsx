72208cbf32a28eedbdef69663c2f82457686fab2
Dawitlior <dawitlior777@gmail.com>
2023-03-27 00:10:47 +0300

    creating styled component

---
+++ client/src/components/FlexBetween.tsx
@@ -0,0 +1,12 @@
+/** styled component */
+
+import {Box} from '@mui/material';
+import { styled } from '@mui/material';
+
+const FlexBetween = styled(Box)({
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+})
+
+export default FlexBetween;
