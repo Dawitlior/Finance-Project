4a3ca6aceeb694318a238eacbf8ac1e8882e2662
Dawitlior <dawitlior777@gmail.com>
2023-03-27 12:22:17 +0300

    configurations, types, chartes, mongoDB connections, redux-toolkit-quary

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -1,14 +1,55 @@
-import React from "react";
+import React, { useMemo } from "react";
 import DashboardBox from "@/components/DashboardBox";
 import { useGetKpisQuery } from "@/state/api";
+import {
+  ResponsiveContainer,
+  AreaChart,
+  CartesianGrid,
+  XAxis,
+  Tooltip,
+  Area,
+  YAxis
+} from "recharts";
 type Props = {};
 
 const Row1 = (props: Props) => {
-    const { data } = useGetKpisQuery();
-    console.log("🚀 ~ file: Row1.tsx:8 ~ Row1 ~ data:", data)
+  const { data } = useGetKpisQuery();
+  console.log("🚀 ~ file: Row1.tsx:8 ~ Row1 ~ data:", data);
+  const revenueExpenses = useMemo(() => {
+    return (
+      data &&
+      data[0].monthlyData.map(({ expenses, revenue, month }) => {
+        return {
+          name: month.substring(0, 3),
+          revenue: revenue,
+          expenses: expenses,
+        }
+      })
+    )
+  }, [])
   return (
     <>
-      <DashboardBox bgcolor="#fff" gridArea="a"></DashboardBox>
+      <DashboardBox bgcolor="#fff" gridArea="a">
+        <ResponsiveContainer width="100%" height="100%">
+          <AreaChart
+            width={500}
+            height={400}
+            data={data}
+            margin={{
+              top: 10,
+              right: 30,
+              left: 0,
+              bottom: 0,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+          </AreaChart>
+        </ResponsiveContainer>
+      </DashboardBox>
       <DashboardBox bgcolor="#fff" gridArea="b"></DashboardBox>
       <DashboardBox bgcolor="#fff" gridArea="c"></DashboardBox>
     </>
